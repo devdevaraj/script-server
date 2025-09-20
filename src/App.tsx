4723d5ae79f79337ad3cec8c7aaa961b1d07b385
Devaraj <devarajperayil327@gmail.com>
2025-09-20 12:30:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+ const copy = () => {
+  navigator.clipboard.writeText(`${location}scripts/hello-world.sh`);
+ }
+ return (
+  <main className="p-4">
+   <h1 className="text-2xl text-center">Scripts</h1>
+   <ol className="w-64">
+    <li className="flex items-center justify-center gap-4">
+     <a
+      className="text-blue-600"
+      href="/scripts/hello-world.sh"
+     >
+      Hello world
+     </a>
+     <button onClick={copy}>
+      copy
+     </button>
+    </li>
+   </ol>
+  </main>
+ )
+}
+
+export default App;
