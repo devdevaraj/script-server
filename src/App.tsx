cded644c416b2ee665d827c4a5ae28db82e1ac3b
Devaraj <devarajperayil327@gmail.com>
2025-10-07 14:12:07 +0530

    Add new script

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
+import toast, { Toaster } from "react-hot-toast";
 import './App.css'
 
 function App() {
  const copy = (script: string) => {
   navigator.clipboard.writeText(`curl -s ${location}${script} | sh`);
+  toast.success("Copied!");
  }
  const scripts = [
   "/hello-world.sh",
   "/run-init.sh",
  ];
  return (
-  <main className="p-4">
-   <h1 className="text-2xl text-center">Scripts</h1>
-   <ol className="w-64">
-    {scripts.map((e) => (
-     <li key={e} className="flex items-center justify-center gap-4">
-      <a
-       className="text-blue-600"
-       href={e}
-      >
-       {e.split("/")[1].split(".sh")[0].replace("-"," ")}
-      </a>
-      <button onClick={() => copy(e)}>
-       copy
-      </button>
-     </li>
-    ))}
-   </ol>
-  </main>
+  <>
+  <Toaster position="top-center"/>
+   <main className="p-4">
+    <h1 className="text-2xl text-center">Scripts</h1>
+    <ol className="w-64">
+     {scripts.map((e) => (
+      <li key={e} className="flex items-center justify-center gap-4">
+       <a
+        className="text-blue-600"
+        href={e}
+       >
+        {e.split("/")[1].split(".sh")[0].replace("-", " ")}
+       </a>
+       <button onClick={() => copy(e)}>
+        copy
+       </button>
+      </li>
+     ))}
+    </ol>
+   </main>
+  </>
  )
 }
 
